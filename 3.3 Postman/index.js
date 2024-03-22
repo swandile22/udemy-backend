4603500acd88fde0d8f20b9ddebd0d32d52435c7
swandile22 <sandile@spitfireinbound.com>
2024-03-22 20:38:58 +0200

    🎓 Middlewares

--- 3.3 Postman/index.js
+++ 3.3 Postman/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 const app = express();
 const port = 3000;
 
@@ -10,28 +11,32 @@ const port = 3000;
 // *********************
 
 app.get("/", (req, res) => {
-  res.send("<h1>Home Page</h1>");
+    res.send("<h1>Home Page</h1>");
 });
 
-app.post("/register", (req, res) => {
-  res.send("<h1>Your information has been stored inside our database</h1>");
-  res.sendStatus(201);
+app.post("/register", (req,
+                       res) => {
+    res.send("<h1>Your information has been stored inside our database</h1>");
+    res.sendStatus(201);
 });
 
-app.put("/user/angela", (req, res) => {
-  res.sendStatus(200);
+app.put("/user/angela", (req,
+                         res) => {
+    res.sendStatus(200);
 });
 
-app.patch("/user/angela", (req, res) => {
-  res.send("<h1>Your First Name has been updated</h1>")
-  res.sendStatus(200);
+app.patch("/user/angela", (req,
+                           res) => {
+    res.send("<h1>Your First Name has been updated</h1>")
+    res.sendStatus(200);
 });
 
-app.delete("/user/angela", (req, res) => {
-  res.send("<h1>Your First Name has been deleted, successfully</h1>")
-  res.sendStatus(200);
+app.delete("/user/angela", (req,
+                            res) => {
+    res.send("<h1>Your First Name has been deleted, successfully</h1>")
+    res.sendStatus(200);
 });
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+    console.log(`Server started on port ${port}`);
 });
