b3a1e67e591b64fbc057c7db0afa991ecb67d90f
swandile22 <sandile@spitfireinbound.com>
2024-03-25 12:24:38 +0200

    🎓 Secrets page

--- 3.4 Middleware/index4.js
+++ 3.4 Middleware/index4.js
@@ -3,19 +3,30 @@ import { dirname } from "path"
 import { fileURLToPath} from "url";
 import bodyParser from "body-parser";
 
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+let bandName = "";
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+function logger(req, res, next) {
+  const receivedData = req.body;
+  bandName = receivedData["street"] + receivedData["pet"];
+  next();
+}
+
+
+app.use(logger);
+
 
 app.get("/submit", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/submit", (req, res) => {
-  console.log(req.body);
-  console.log(req.body(this.street))
+  res.send(`<h1>Here's your generated band name:</h1><em><h2>${bandName}🤘</h2></em>`)
 
 });
 app.listen(port, () => {
---
+++ 3.5 Secrets Project/index.js
@@ -0,0 +1,36 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express from "express";
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3005
+const password = "ILoveProgramming";
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/check", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+});
+
+app.post("/check", (req, res) => {
+    // if (req.body["password"] !== password) {
+    //     return 0;
+    // } else {
+    //     console.log("Correct!");
+    // }
+    if (req.body["password"] !== password) {
+        res.sendFile(__dirname + "/public/index.html");
+    } else {
+        res.sendFile(__dirname + "/public/secret.html")
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
