098842f205d4fe28edd3d7391fbcd6d68ec9241e
swandile22 <sandile@spitfireinbound.com>
2024-04-07 19:03:36 +0200

    🎓 Embedded Javascript - Capstone Project

--- CapstoneProjectBlogEJS/index.js
+++ CapstoneProjectBlogEJS/index.js
@@ -3,29 +3,19 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 5400;
-let bandName = "";
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-// function logger(req, res, next) {
-//   const receivedData = req.body;
-//   bandName = receivedData["street"] + receivedData["pet"];
-//   next();
-// }
 
 app.get("/", (req, res)=> {
     res.render("index.ejs");
 });
 
 app.post("/submit", (req, res)=>{
-   const formHeader = req.body["blogHeader"];
-   const formSubheader = req.body["blogSubHeader"];
-   const formFile = req.body["fileUpload"];
-   const formUrl = req.body["blogUrl"];
-    console.log(formHeader);
-    console.log(formSubheader);
-    console.log(formFile);
-    console.log(formUrl);
+    const blogThumbnail = req.body["fileUpload"];
+    const blogHeader = req.body["blogHeader"];
+    const blogSubheader = req.body["blogSubHeader"];
+    const blogURL = req.body["blogUrl"];
+   res.render("index.ejs", {thumbnail:blogThumbnail,
+       headerText:blogHeader, subHeader: blogSubheader, URL:blogURL})
 });
 
 app.listen(port, ()=>{
