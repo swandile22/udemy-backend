843118c4ad23f7345ac38b9b8b99cec4e8959652
swandile22 <sandile@spitfireinbound.com>
2024-04-07 19:37:01 +0200

    🎓 Embedded Javascript - Capstone Project(multer)

--- CapstoneProjectBlogEJS/index.js
+++ CapstoneProjectBlogEJS/index.js
@@ -1,15 +1,27 @@
 import express from 'express';
 import bodyParser from "body-parser";
+import path from 'path';
+import multer from 'multer';
 
 const app = express();
 const port = 5400;
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const storage = multer.diskStorage({
+    destination: (res, file, cb) => {
+        cb(null, "public/images");
+    }, filename: (res, file, cb) => {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname))
+    }});
+
+const upload = multer({storage:storage});
+
 app.get("/", (req, res)=> {
     res.render("index.ejs");
 });
 
-app.post("/submit", (req, res)=>{
+app.post("/submit", upload.single("fileUpload"), (req, res)=>{
     const blogThumbnail = req.body["fileUpload"];
     const blogHeader = req.body["blogHeader"];
     const blogSubheader = req.body["blogSubHeader"];
